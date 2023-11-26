ef6d45c6f35fab1b313100e4817f9785ec2345d1
wc-0904 <wasmirchowdhury@gmail.com>
2023-11-25 21:01:17 -0500

    Added image opening code, png files for sprites

---
+++ openingImage_v1.0.py
@@ -0,0 +1,50 @@
+from cmu_graphics import *
+from PIL import Image
+
+FILENAME = 'images/sample-spritestrip.png'
+OUTPUT_SIZE = (6*50, 50)
+
+input_spritesheet = Image.open(FILENAME)
+print(f'Size: {input_spritesheet.size}')
+
+resized_spritesheet = input_spritesheet.resize((OUTPUT_SIZE[0], OUTPUT_SIZE[1]))
+
+resized_spritesheet.save('resized_spritesheet.png')
+
+# Image.new("RGB", (OUTPUT_SIZE[0]*gif.n_frames, OUTPUT_SIZE[1]))
+# gif.show()
+
+# for frame in range(0, gif.n_frames):
+#     gif.seek(frame)
+#     extracted = gif.resize(OUTPUT_SIZE)
+#     position = (frame*OUTPUT_SIZE[0], 0)
+#     sprite_sheet.paste(extracted, position)
+
+# sprite_sheet.save("spritesheet.bmp")
+
+
+def onAppStart(app):
+    app.spriteCounter = 0
+    app.stepCount = 0
+    app.stepsPerSecond = 10
+
+    player_spritesheet = Image.open('resized_spritesheet.png')
+
+    app.sprites = []
+    for i in range(6):
+        sprite = CMUImage(player_spritesheet.crop((50*i, 0, 50*(i+1), 50)))
+        app.sprites.append(sprite)
+
+def onStep(app):
+    app.stepCount +=1
+    # if ___
+    app.spriteCounter = (1+app.spriteCounter) % len(app.sprites)
+
+def redrawAll(app):
+    currSprite = app.sprites[app.spriteCounter]
+    drawImage(currSprite, app.width/2, app.height/2)
+
+def main():
+    runApp(width=400, height=400)
+
+main()
